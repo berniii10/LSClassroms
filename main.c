5abc87a3568c32e4203c65a5d98d9f58d9f165db
Bernat Oller <bernatollertech@gmail.com>
2022-04-01 15:45:47 +0200

    Fitxers added

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+
+
 int main() {
+
 
     printf("Welcome!\n\n");
 
     printf("Type the name of the 'classrooms' file: ");
+
+    printf("\nType the name of the 'students' file: ");
+
+    printf("\n\n");
+
 
 }
